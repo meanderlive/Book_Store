d7bf5e42f650009d71caef0ca4696a6369466cf6
Agam <javascript.mspl@gmail.com>
2025-07-21 18:24:38 +0530

    new codebase

--- Frontend/src/context/AuthProvider.jsx
+++ Frontend/src/context/AuthProvider.jsx
@@ -5,7 +5,7 @@ export default function AuthProvider({ children }) {
   const initialAuthUser = localStorage.getItem("Users");
   const [authUser, setAuthUser] = useState(
     initialAuthUser ? JSON.parse(initialAuthUser) : undefined
-  );
+  ); 
   return (
     <AuthContext.Provider value={[authUser, setAuthUser]}>
       {children}
